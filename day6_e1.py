a6197d7bf505700a6145d5e5a528735668de5a68
Max Lamparth <max.lamparth@tum.de>
2021-03-11 20:11:23 +0100

    Fixed metropolis algorithm for missing acceptance case and a few pep8 related changes.

--- day6_e1.py
+++ day6_e1.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from mathfunctions import gaussian
 
 
-def metropolis(func, params, n_trials: int = 1000, prop_rng=None, x0: float = 0.):
+def metropolis(func, params, n_trials: int = 1000, prop_rng=None, x0: float = 0.0):
     """Metropolis Algorithm"""
 
     if prop_rng is None:
-        prop_rng = [-1., 1.]
+        prop_rng = [-1.0, 1.0]
 
     x_curr = x0
     accept = []
@@ -18,11 +18,13 @@ def metropolis(func, params, n_trials: int = 1000, prop_rng=None, x0: float = 0.
         u_2 = np.random.uniform()
         y_curr = x_curr + u_1
 
-        rho = min(func(y_curr, **params)/func(x_curr, **params), 1.)
+        rho = min(func(y_curr, **params) / func(x_curr, **params), 1.0)
 
         if u_2 <= rho:
             accept.append(y_curr)
             x_curr = y_curr
+        else:
+            accept.append(x_curr)
 
     accept = np.asarray(accept)
     efficiency = accept.shape[0] / n_trials
@@ -33,41 +35,37 @@ def metropolis(func, params, n_trials: int = 1000, prop_rng=None, x0: float = 0.
 
 n_sim = 100000
 tests = [
-    [[-1., 1.], 0.],
-    [[-0.5, 0.5], 0.],
-    [[-0.1, 0.1], 0.],
-    [[-3., 3.], 0.],
-    [[-1., 1.], 1.],
-    [[-1., 0.5], 0.5]
+    [[-1.0, 1.0], 0.0],
+    [[-0.5, 0.5], 0.0],
+    [[-0.1, 0.1], 0.0],
+    [[-3.0, 3.0], 0.0],
+    [[-1.0, 1.0], 1.0],
+    [[-1.0, 0.5], 0.5],
 ]
 
 for test in tests:
     res = metropolis(
-        gaussian,
-        {"mu": 0., "sig": 1.},
-        n_sim,
-        prop_rng=test[0],
-        x0=test[1]
+        gaussian, {"mu": 0.0, "sig": 1.0}, n_sim, prop_rng=test[0], x0=test[1]
     )
     test.append(res[0])
     test.append(res[1])
 
 fig, axs = plt.subplots(3, 2, sharex=True, sharey=True)
-fig.suptitle(f"Metropolis Gaussian Tests - {n_sim} Tterations")
+fig.suptitle(f"Metropolis Gaussian Tests - {n_sim} Iterations")
 x_vals = np.linspace(-4.5, 4.5, 1000)
 bins = int(n_sim * 0.001)
 
 for test_i, test in enumerate(tests):
-    axs.flat[test_i].hist(test[2], bins=bins, range=[-4.5, 4.5], label="gen. RN", density=True)
+    axs.flat[test_i].hist(
+        test[2], bins=bins, range=[-4.5, 4.5], label="gen. RN", density=True
+    )
     axs.flat[test_i].plot(x_vals, gaussian(x_vals), label="Unit. Gaussian")
     axs.flat[test_i].legend()
     axs.flat[test_i].set_xlabel("x [ ]")
     axs.flat[test_i].set_ylabel("a.u. [ ]")
     axs.flat[test_i].set_title(f"Test {test_i + 1}")
     axs.flat[test_i].annotate(
-        f"prop. rng = {test[0]} \n"
-        f"x0 = {test[1]}\n"
-        f"eff = {test[3]:0.2}",
+        f"prop. rng = {test[0]} \n" f"x0 = {test[1]}\n" f"eff = {test[3]:0.2}",
         xy=(0.05, 0.95),
         xycoords="axes fraction",
         ha="left",
@@ -76,5 +74,3 @@ for test_i, test in enumerate(tests):
     )
 
 plt.show()
-
-
