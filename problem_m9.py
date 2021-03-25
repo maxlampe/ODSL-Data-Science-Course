194869322e83325519d5109641079de83d4783a2
Max Lamparth <max.lamparth@tum.de>
2021-03-25 13:19:32 +0100

    feat: Simplified annealing again, as I don't think that that is the issue. feat: Overhauled plotting. feat: Added distance matrix to speed up calculations.

--- problem_m9.py
+++ problem_m9.py
@@ -10,12 +10,14 @@ class M9Map:
     def __init__(self, n_towns: int, sim_range: list = [0.0, 1.0]):
         self.n_towns = n_towns
         self.sim_range = sim_range
+
         self.map = self._gen_map()
         self.order = np.linspace(0, self.n_towns - 1, self.n_towns, dtype=int)
+        self.dist_mat = np.ones([self.n_towns, self.n_towns])
+        self._calc_dist_mat()
         self.curr_dist = self._calc_total_dist(self.order)
 
         self.cache = []
-        self.lazy = 0
 
     def _gen_map(self):
         """Generate a number of cities with coordinates"""
@@ -28,19 +30,35 @@ class M9Map:
         """Plot map of towns"""
 
         plot_map = self.map[self.order].T
-        # plot_map = np.append(plot_map, self.map[self.order[0]]).T
-        plt.plot(plot_map[0], plot_map[1], label="Path")
-        plt.plot(plot_map[0], plot_map[1], ".", label="T")
+
+        fig, axs = plt.subplots(1, 1, figsize=(8, 8))
+        fig.suptitle("Traveling salesman problem")
+
+        axs.plot(plot_map[0], plot_map[1], label="Path")
+        axs.plot(
+            [plot_map[0][-1], plot_map[0][0]],
+            [plot_map[1][-1], plot_map[1][0]],
+            label="Final step",
+        )
+        axs.plot(plot_map[0], plot_map[1], ".", label="Towns")
+        axs.set_xlabel("x-coord. [ ]")
+        axs.set_ylabel("y-coord. [ ]")
+
+        delta_range = self.sim_range[1] - self.sim_range[0]
+        axs.set_xlim(
+            0.9 * self.sim_range[0] - 0.1 * delta_range,
+            1.1 * self.sim_range[1] + 0.1 * delta_range,
+        )
+        axs.set_ylim(
+            0.9 * self.sim_range[0] - 0.1 * delta_range,
+            1.1 * self.sim_range[1] + 0.1 * delta_range,
+        )
+
         i = 0
         for xy in zip(plot_map[0], plot_map[1]):
-            plt.annotate(f"T{i}", xy=xy, textcoords="data")
+            axs.annotate(f"T{i}", xy=xy, textcoords="data")
             i += 1
 
-        plt.xlabel("x-coord. [ ]")
-        plt.ylabel("y-coord. [ ]")
-        plt.title("Traveling salesman problem")
-        plt.xlim(-0.1, 1.1)
-        plt.ylim(-0.1, 1.1)
         plt.legend()
         if bsave_fig:
             plt.savefig("output/problem_9_map.png", dpi=300)
@@ -54,89 +72,72 @@ class M9Map:
 
         return np.sqrt((t1[0] - t2[0]) ** 2 + (t1[1] - t2[1]) ** 2)
 
+    def _calc_dist_mat(self):
+        """Calculate distance matrix between all towns as LUT"""
+        for i in range(self.n_towns):
+            for j in range(self.n_towns):
+                self.dist_mat[i, j] = self._get_distances(i, j)
+
     def _calc_total_dist(self, order: np.array):
         """Calculate distance for current order"""
 
         total_dist = 0
-        for i in range(order.shape[0] - 1):
-            total_dist += self._get_distances(order[i], order[i + 1])
-        total_dist += self._get_distances(order[-1], order[0])
+        n = order.shape[0]
+        for i in range(order.shape[0]):
+            total_dist += self.dist_mat[order[i % n], order[(i + 1) % n]]
 
         return total_dist
 
-    def _do_iteration(self, temp: float):
-        """Do one iteration of the MCMC algorithm"""
+    def _do_step(self, temp: float):
+        """Do one step of the MCMC algorithm"""
 
-        inter_order = self.order
-        u = round(np.random.uniform(0, self.n_towns - 1))
+        tmp_order = self.order
+        u = np.random.randint(0, self.n_towns)
+        v1 = u % self.n_towns
+        v2 = (u + 1) % self.n_towns
 
-        if u < self.n_towns - 1:
-            inter_val = inter_order[u]
-            inter_order[u] = inter_order[u + 1]
-            inter_order[u + 1] = inter_val
-        else:
-            inter_val = inter_order[u]
-            inter_order[u] = inter_order[0]
-            inter_order[0] = inter_val
+        tmp_order[v1], tmp_order[v2] = tmp_order[v2], tmp_order[v1]
+        tmp_dist = self._calc_total_dist(tmp_order)
 
-        inter_dist = self._calc_total_dist(inter_order)
-        if inter_dist < self.curr_dist:
-            self.order = inter_order
-            self.curr_dist = inter_dist
-        else:
-            p = np.random.uniform()
-            if p < (np.exp(-inter_dist / temp)):
-                self.order = inter_order
-                self.curr_dist = inter_dist
-            else:
-                self.lazy += 1
+        p = np.random.uniform()
+        delta_d = tmp_dist - self.curr_dist
+        if p < min(1.0, np.exp(-delta_d / temp) / temp):
+            self.order = tmp_order
+            self.curr_dist = tmp_dist
 
         self.cache.append(np.asarray([self.curr_dist, temp]))
 
-    def do_MCMC(self, n_sim: int = 100000, temp: float = 10.0):
+    def do_mcmc(self, n_sim: int = 1000000, temp: float = 10000.0, bplot_sim: bool = True):
         """Run the Markov-Chain-Monte-Carlo"""
 
         # Reset cache
         self.cache = []
-        self.lazy = 0
         t = temp
-        count_same = 0
-        count_n = 0
-        last_dist = self.curr_dist
+
         for n in range(n_sim):
-            # if (n % 10000) == 0 and n > 0:
-            #     t *= 0.5
-            self._do_iteration(t)
-
-            if np.abs(last_dist - self.curr_dist) < 0.0001:
-                count_same += 1
-            if count_same > 10000:
-                t *= 0.5
-                count_n = 0
-                count_same = 0
-            else:
-                count_n += 1
-            if count_n > 15000:
-                t *= 0.5
-                count_n = 0
-                count_same = 0
-
-            last_dist = self.curr_dist
+            self._do_step(t)
+            if n % 50000 == 0 and n > 0:
+                t *= 0.1
 
         self.cache = np.asarray(self.cache)
-        print(self.lazy, n_sim)
 
-        fig, axs = plt.subplots(1, 2, figsize=(15, 8))
+        if bplot_sim:
+            fig, axs = plt.subplots(2, 1, figsize=(9, 14), sharex=True)
+            fig.suptitle("Simulation run")
+            axs[0].plot(self.cache.T[0])
+            axs[1].plot(self.cache.T[1])
+            axs[1].set_yscale("log")
+            axs[1].set_xlabel("Sim steps [ ]")
+            axs[0].set_ylabel("Total distance [ ]")
+            axs[1].set_ylabel("Temp [ ]")
+            fig.subplots_adjust(hspace=0.01)
+            plt.tight_layout()
+            plt.show()
 
-        axs[0].plot(self.cache.T[0])
-        axs[1].plot(self.cache.T[1])
-        axs[1].set_yscale("log")
-        plt.show()
 
-
-map_class = M9Map(n_towns=25)
+map_class = M9Map(n_towns=19)
 
 print(map_class.curr_dist)
-map_class.do_MCMC()
-print(map_class.order, map_class.curr_dist)
+map_class.do_mcmc()
+print(map_class.order, "\n", map_class.curr_dist)
 map_class.plot_map()
